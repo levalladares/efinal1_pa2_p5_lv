d44b53b2ab8adb27d371c70b8caab8a18964c643
Luis <levalladares@uce.edu.ec>
2024-01-15 19:59:00 -0500

    Examen2 changes

--- src/main/java/com/example/demo/Efinal1Pa2P5LvApplication.java
+++ src/main/java/com/example/demo/Efinal1Pa2P5LvApplication.java
@@ -31,36 +31,36 @@ public class Efinal1Pa2P5LvApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		Propietario p1 = new Propietario();
-		p1.setNombre("Luis");
-		p1.setApellido("Valladares");
-		p1.setCedula("78485454");
+//		Propietario p1 = new Propietario();
+//		p1.setNombre("Luis");
+//		p1.setApellido("Valladares");
+//		p1.setCedula("78485454");
+//		
+//		CuentaBancaria c1= new CuentaBancaria();
+//		c1.setNumero("0123456");
+//		c1.setPropietario(p1);
+//		c1.setSaldo(new BigDecimal(100));
+//		c1.setTipo("ahorros");
+//		this.bancariaService.guardar(c1);
+//		
+//		Propietario p2 = new Propietario();
+//		p2.setNombre("Jose");
+//		p2.setApellido("Bastidas");
+//		p2.setCedula("78485474");
+//		
+//		CuentaBancaria c2= new CuentaBancaria();
+//		c2.setNumero("6543210");
+//		c2.setPropietario(p2);
+//		c2.setSaldo(new BigDecimal(50));
+//		c2.setTipo("ahorros");
+//		this.bancariaService.guardar(c2);
+//		
+//		this.iTransferenciaService.guardar("0123456", "6543210", new BigDecimal(20));
 		
-		CuentaBancaria c1= new CuentaBancaria();
-		c1.setNumero("123456");
-		c1.setPropietario(p1);
-		c1.setSaldo(new BigDecimal(100));
-		c1.setTipo("ahorros");
-		//this.bancariaService.guardar(c1);
-		
-		Propietario p2 = new Propietario();
-		p2.setNombre("Jose");
-		p2.setApellido("Bastidas");
-		p2.setCedula("78485474");
-		
-		CuentaBancaria c2= new CuentaBancaria();
-		c2.setNumero("654321");
-		c2.setPropietario(p2);
-		c2.setSaldo(new BigDecimal(50));
-		c2.setTipo("ahorros");
-		//this.bancariaService.guardar(c2);
-		
-		this.iTransferenciaService.guardar("123456", "654321", new BigDecimal(20));
-		
-		CuentaBancaria cta= this.bancariaService.buscararPorNumero("123456");
+		CuentaBancaria cta= this.bancariaService.buscararPorNumero("0123456");
 		System.out.println(cta.getSaldo());
 		
-		CuentaBancaria cta2= this.bancariaService.buscararPorNumero("654321");
+		CuentaBancaria cta2= this.bancariaService.buscararPorNumero("6543210");
 		System.out.println(cta2.getSaldo());
 		
 		
